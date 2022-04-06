a57c46c9c27562a78508c4626e0ed5a8e63a6dd9
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-06 10:57:55 -0600

    Create From structure

--- src/components/NewTrickForm/NewTrickForm.js
+++ src/components/NewTrickForm/NewTrickForm.js
@@ -1,2 +1,42 @@
 import React, { Component } from 'react';
 import './NewTrickFrom.css';
+
+class NewTrickFrom extends Component {
+  constructor() {
+    super()
+    this.state = {
+      stance: '',
+      name: '',
+      obstacle: '',
+      link: ''
+    }
+  }
+  render() {
+    return (
+      <form className='new-trick-form'>
+        <select name='stance'>
+          <option value=''>Select Your Stance</option>
+          <option value='switch'>Switch</option>
+          <option value='regular'>Regular</option>
+        </select>
+        <label for='name'>
+          <input type='text' name='name' placeholder='Name of Trick'>
+        </label>
+        <select name='obstacle'>
+          <option value=''>Choose your Obstacle</option>
+          <option value='flatground'>Flatground</option>
+          <option value='ledge'>Ledge</option>
+          <option value='rail'>Rail</option>
+          <option value='stairs'>Stairs</option>
+          <option value='pool'>Pool</option>
+        </select>
+        <label for='link'>
+          <input type='text' name='link' placeholder='Link to Tutorial'>
+        </label>
+      </form>
+    )
+  }
+}
+
+
+'Flatground', 'Ledge', 'Rail', 'Stairs', and 'Pool'
