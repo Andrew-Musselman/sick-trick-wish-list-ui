38d5f43e9baeef6154f1dbc4f3ea4a75bb3b50c3
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-06 09:48:21 -0600

    import Tricks into app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,19 @@
 import { Component } from 'react';
+import Tricks from '../tricks/Tricks'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      ticks = []
+      ticks: []
     }
   }
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+        <Tricks ticks={this.state.tricks}/>
       </div>
     );
   }
