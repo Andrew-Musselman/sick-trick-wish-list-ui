0be56f4817ca3238287b5838b6700c885ad922bc
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-06 10:10:33 -0600

    Add missing closing p tag

--- src/components/TrickCard/TrickCard.js
+++ src/components/TrickCard/TrickCard.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './TrickCard.css';
 
 
-const TrickCard = ({ stance, obstacle, tutorial, id}) => {
+const TrickCard = ({ stance, name, obstacle, tutorial, id }) => {
   return (
-    <>
-
-    </>
+    <div className='trick-card'>
+      <p>{stance} {name}</p>
+      <p>Obstacle: {obstacle}</p>
+      <p>Link to Tutorial:</p>
+      <p>{tutorial}</p>
+    </div>
   )
 }
 
