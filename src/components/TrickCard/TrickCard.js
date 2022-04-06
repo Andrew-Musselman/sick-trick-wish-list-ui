14f116679f4fee11f6a08824a1ae30ef66661567
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-06 10:32:49 -0600

    Change link from p tag to a

--- src/components/TrickCard/TrickCard.js
+++ src/components/TrickCard/TrickCard.js
@@ -8,7 +8,7 @@ const TrickCard = ({ stance, name, obstacle, tutorial, id }) => {
       <p>{stance} {name}</p>
       <p>Obstacle: {obstacle}</p>
       <p>Link to Tutorial:</p>
-      <p>{tutorial}</p>
+      <a href={tutorial}>{tutorial}</a>
     </div>
   )
 }
