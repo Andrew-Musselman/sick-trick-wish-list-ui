16a38bee0ad2362e8606d35f2bb4bda9b8e6e345
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-06 11:14:32 -0600

    Add handle change method to new trick form and elements

--- src/components/NewTrickForm/NewTrickForm.js
+++ src/components/NewTrickForm/NewTrickForm.js
@@ -11,18 +11,28 @@ class NewTrickForm extends Component {
       link: ''
     }
   }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault
+
+  }
+
   render() {
     return (
       <form className='new-trick-form'>
-        <select name='stance'>
+        <select name='stance' value={this.state.stance} onChange={this.handleChange}>
           <option value=''>Select Your Stance</option>
           <option value='switch'>Switch</option>
           <option value='regular'>Regular</option>
         </select>
-        <label for='name'>
-          <input type='text' name='name' placeholder='Name of Trick'>
+        <label htmlFor='name'>
+          <input type='text' name='name' placeholder='Name of Trick' value={this.state.name} onChange={this.handleChange}/>
         </label>
-        <select name='obstacle'>
+        <select name='obstacle' value={this.state.obstacle} onChange={this.handleChange}>
           <option value=''>Choose your Obstacle</option>
           <option value='flatground'>Flatground</option>
           <option value='ledge'>Ledge</option>
@@ -30,9 +40,10 @@ class NewTrickForm extends Component {
           <option value='stairs'>Stairs</option>
           <option value='pool'>Pool</option>
         </select>
-        <label for='link'>
-          <input type='text' name='link' placeholder='Link to Tutorial'>
+        <label htmlFor='link'>
+          <input type='text' name='link' placeholder='Link to Tutorial' value={this.state.link} onChange={this.handleChange}/>
         </label>
+        <button onClick={this.handleSubmit}>Send It!</button>
       </form>
     )
   }
