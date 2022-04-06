4f1b6d19ac1ffc17d47af1fa5b31a3ad20d757ec
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-06 09:52:31 -0600

    Map over tricks to pass props to each Trick Card

--- src/components/tricks/Tricks.js
+++ src/components/tricks/Tricks.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './Tricks.css';
 
 const Tricks = ({ tricks }) => {
+  const trickCards = tricks.map(trick => {
+    <Trick
+      stance ={trick.stance}
+      obstacle ={trick.obstacle}
+      tutorial ={trick.tutorial}
+      id ={trick.id}
+      key ={trick.id}
+  })
   return (
 
   )
