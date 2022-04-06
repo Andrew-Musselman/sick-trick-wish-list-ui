586f3acd792a9a5670957da39caf061552e1ce92
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-06 10:00:41 -0600

    Create component structure for TrickCard component

--- src/components/TrickCard/TrickCard.js
+++ src/components/TrickCard/TrickCard.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './TrickCard.css';
+
+
+const TrickCard = ({ stance, obstacle, tutorial, id}) => {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default TrickCard;
