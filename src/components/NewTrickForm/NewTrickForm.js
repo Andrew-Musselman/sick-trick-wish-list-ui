de52dbb5c5d5863e7ce7767ac48bd12438ee0d79
Andrew Musselman <andrew.mussels@gmail.com>
2022-04-06 11:41:43 -0600

    Add event to onchange in form

--- src/components/NewTrickForm/NewTrickForm.js
+++ src/components/NewTrickForm/NewTrickForm.js
@@ -33,15 +33,15 @@ class NewTrickForm extends Component {
   render() {
     return (
       <form className='new-trick-form'>
-        <select name='stance' value={this.state.stance} onChange={this.handleChange}>
+        <select name='stance' value={this.state.stance} onChange={event => this.handleChange(event)}>
           <option value=''>Select Your Stance</option>
           <option value='switch'>Switch</option>
           <option value='regular'>Regular</option>
         </select>
         <label htmlFor='name'>
-          <input type='text' name='name' placeholder='Name of Trick' value={this.state.name} onChange={this.handleChange}/>
+          <input type='text' name='name' placeholder='Name of Trick' value={this.state.name} onChange={event => this.handleChange(event)}/>
         </label>
-        <select name='obstacle' value={this.state.obstacle} onChange={this.handleChange}>
+        <select name='obstacle' value={this.state.obstacle} onChange={event => this.handleChange(event)}>
           <option value=''>Choose your Obstacle</option>
           <option value='flatground'>Flatground</option>
           <option value='ledge'>Ledge</option>
@@ -50,7 +50,7 @@ class NewTrickForm extends Component {
           <option value='pool'>Pool</option>
         </select>
         <label htmlFor='link'>
-          <input type='text' name='link' placeholder='Link to Tutorial' value={this.state.link} onChange={this.handleChange}/>
+          <input type='text' name='link' placeholder='Link to Tutorial' value={this.state.link} onChange={event => this.handleChange(event)}/>
         </label>
         <button onClick={event => this.handleSubmit(event)}>Send It!</button>
       </form>
